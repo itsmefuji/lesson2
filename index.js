6b0944ab2e846fb50f9044b6b6f9151f6ef2f306
itsmefuji <yujif.gc@gmail.com>
2016-01-21 15:00:01 -0500

    updated index.ja

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var http = require('http');
 var hostname = '127.0.0.1';
-var port = 1337;
+var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port, hostname, function () {
-    console.log("Server running at http://" + hostname + ":" + port + "/");
-});
+}).listen(port);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
 var http = require('http');
 
 var hostname = '127.0.0.1';
-var port = 1337;
+var port:number = process.env.PORT || 1337;
 
-http.createServer(function(req, res) {
+http.createServer(function(req:http.ServerRequest, res:http.ServerResponse) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port, hostname, function()  {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+}).listen(port);
