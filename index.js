71be1814b141e1560c3a65e7654823784f8acd47
itsmefuji <yujif.gc@gmail.com>
2016-01-21 15:08:28 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var hostname = '127.0.0.1';
 var port = process.env.PORT || 1337;
+//varport:number = process.env.PORT || 1337; 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ var http = require('http');
 
 var hostname = '127.0.0.1';
 var port:number = process.env.PORT || 1337;
+//varport:number = process.env.PORT || 1337; 
 
 http.createServer(function(req:http.ServerRequest, res:http.ServerResponse) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
